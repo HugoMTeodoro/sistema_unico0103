4e2123a457307733752ff306d7c79abbca38d726
HugoMTeodoro <hugomteodoro1@gmail.com>
2021-03-15 20:55:47 -0300

    logins para entrega 15/03

--- public/doctor/deleteDoctor.php
+++ public/doctor/deleteDoctor.php
@@ -1,29 +1,28 @@
-<?php include("../auth/validarADM.php")?>
+<?php include("../auth/validarADM.php") ?>
 
 
 
-<?php 
-        include_once("../../data/db_connection.php");
-        if (isset ($_GET["crm"])) {
-            $sql = "DELETE FROM medico WHERE CRM = " . $_GET["crm"];
+<?php
+include_once("../../data/db_connection.php");
+if (isset($_GET["crm"])) {
+    $sql = "DELETE FROM medico WHERE CRM = " . $_GET["crm"];
 
-            if ($connection -> query($sql) === TRUE) {
- ?>
-                <script>
-            alert ("Médico excluído com sucesso");
+    if ($connection->query($sql) === TRUE) {
+?>
+        <script>
+            alert("Médico excluído com sucesso");
             window.location = "listDoctor.php";
-                </script>
+        </script>
 
-                <?php
-            
+    <?php
 
-            }
-        else {
-               ?>
-            <script>
+
+    } else {
+    ?>
+        <script>
             alert("Erro ao excluir o registro");
             window.location = "listDoctor.php";
-            </script> 
+        </script>
 <?php
 
     }
